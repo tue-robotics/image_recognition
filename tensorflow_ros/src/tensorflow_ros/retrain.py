57b14efbd1b9ac28bf5d8496ccd2616121019c51
Ramon Wijnands <rayman747@hotmail.com>
2016-11-12 14:54:26 +0100

    Add try except if path already exists

--- tensorflow_ros/src/tensorflow_ros/retrain.py
+++ tensorflow_ros/src/tensorflow_ros/retrain.py
@@ -7,17 +7,28 @@ from __future__ import print_function
 import os
 from argparse import Namespace
 
+import errno
 import tensorflow as tf
 from tensorflow_ros.tf_retrain import main as tf_main
 
 defaults = Namespace(steps=1000, batch=100)
 
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
 def main(image_dir, model_dir, output_dir, steps, batch):
     tf.app.flags.FLAGS.image_dir = image_dir
     tf.app.flags.FLAGS.model_dir = model_dir
 
-    os.makedirs(output_dir)
+    mkdir_p(output_dir)
     tf.app.flags.FLAGS.output_graph = os.path.join(output_dir, 'output_graph.pb')
     tf.app.flags.FLAGS.output_labels = os.path.join(output_dir, 'output_labels.txt')
 
