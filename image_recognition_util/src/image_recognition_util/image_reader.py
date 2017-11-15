61517b56f872c727f70a551b7c29bc05310b79e6
Loy van Beek <loy.vanbeek@gmail.com>
2017-11-15 19:54:04 +0100

    Documentation touchup

--- image_recognition_util/src/image_recognition_util/image_reader.py
+++ image_recognition_util/src/image_recognition_util/image_reader.py
@@ -8,7 +8,7 @@ def read_annotated(dir_path, patterns=["*.jpg", "*.png", "*.jpeg"]):
     Read annotated images from a directory. This reader assumes that the images in this directory are separated in
     different directories with the label name as directory name. The method returns a generator of the label (string)
     and the opencv image.
-    :param dir_path: The base directory we are going to write read
+    :param dir_path: The base directory we are going to read
     :param patterns: Patterns of the images the reader should match
     """
     for label in os.listdir(dir_path):
