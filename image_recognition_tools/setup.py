e7d46d73d6b115b13354d98316a9579beb2d7fc5
Loy van Beek <loy.vanbeek@gmail.com>
2017-11-23 21:02:26 +0100

    Also specify package dir

--- image_recognition_tools/setup.py
+++ image_recognition_tools/setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['image_recognition_tools']
+    packages=['image_recognition_tools'],
+    package_dir={'': 'src'}
 )
 
 setup(**d)
