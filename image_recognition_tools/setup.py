26d848eedb3bfe2f7dd6e3285d7b18a04fa2c3f2
Loy <LoyVanBeek@users.noreply.github.com>
2017-12-06 09:38:36 +0100

    There is no package, only a script
    I thought I needed this to get the build working

--- image_recognition_tools/setup.py
+++ image_recognition_tools/setup.py
@@ -1,9 +1,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    packages=['image_recognition_tools'],
-    package_dir={'': 'src'}
-)
+d = generate_distutils_setup()
 
 setup(**d)
