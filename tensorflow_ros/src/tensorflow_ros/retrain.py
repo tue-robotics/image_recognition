f84891e669951818404d17187e8e8e2705658278
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-01-02 22:06:49 +0100

    sync tf_retrain, evaluation doesn't work yet with mobilenet

--- tensorflow_ros/src/tensorflow_ros/retrain.py
+++ tensorflow_ros/src/tensorflow_ros/retrain.py
@@ -24,13 +24,15 @@ def mkdir_p(path):
             raise
 
 
-def main(image_dir, model_dir, output_dir, steps, batch, flip_left_right=False, random_crop=0, random_scale=0, random_brightness=0):
+def main(image_dir, model_dir, output_dir, steps, batch, architecture="inception_v3", flip_left_right=False, random_crop=0, random_scale=0, random_brightness=0):
     tf.app.flags.FLAGS.image_dir = image_dir
     tf.app.flags.FLAGS.model_dir = model_dir
 
     mkdir_p(output_dir)
     tf.app.flags.FLAGS.output_graph = os.path.join(output_dir, 'output_graph.pb')
     tf.app.flags.FLAGS.output_labels = os.path.join(output_dir, 'output_labels.txt')
+
+    tf.app.flags.FLAGS.architecture = architecture
 
     tf.app.flags.FLAGS.how_many_training_steps = steps
 
