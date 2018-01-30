b3b852b43a208a19436b332fc4575d1c4fa3bc38
Loy van Beek <loy.vanbeek@gmail.com>
2018-01-30 22:51:26 +0100

    Add a bunch of TODOs

--- openpose_ros/src/openpose_diagnostic_updater.cpp
+++ openpose_ros/src/openpose_diagnostic_updater.cpp
@@ -9,10 +9,12 @@ OpenposeDiagnosticUpdater::OpenposeDiagnosticUpdater()
     // TODO: Get expected frequency from parameter server
     service_diagnostic_ = new diagnostic_updater::FrequencyStatus(
             diagnostic_updater::FrequencyStatusParam(&expected_servicecall_frequency_, &expected_servicecall_frequency_, 0.1));
+
+    //TODO: Add stuff like https://github.com/jackal/jackal_robot/blob/indigo-devel/jackal_base/src/jackal_diagnostic_updater.cpp#L50 :-)
 }
 
 void OpenposeDiagnosticUpdater::tick()
 {
     service_diagnostic_->tick();
-    update();
+    update(); // TODO: This needs to be called at some predefined interval, eg. on a ROS Timer
 }
