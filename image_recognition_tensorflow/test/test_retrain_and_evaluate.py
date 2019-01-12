fe3df2cd5255584dd5dc469fbd7da8ec2242ba26
Rein Appeldoorn <reinzor@gmail.com>
2019-01-12 14:58:16 +0100

    Revert "use rostest"
    This reverts commit 4d4a2997b4c54ea00f25a6e0f71024c671d2857c.

--- image_recognition_tensorflow/test/test_retrain_and_evaluate.py
+++ image_recognition_tensorflow/test/test_retrain_and_evaluate.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python
 
 import os
-import unittest
 
 import rospkg
-import rostest
+
+pkg_name = 'image_recognition_tensorflow'
+pkg_path = rospkg.RosPack().get_path(pkg_name)
+models_path = os.path.join(pkg_path, 'test/models')
+assets_path = os.path.join(pkg_path, 'test/assets')
 
 
-class TestRetrainAndEvaluate(unittest.TestCase):
-    def test_retrain_and_evaluate(self):
-        pkg_name = 'image_recognition_tensorflow'
-        pkg_path = rospkg.RosPack().get_path(pkg_name)
-        models_path = os.path.join(pkg_path, 'test/models')
-        assets_path = os.path.join(pkg_path, 'test/assets')
-        self.assertFalse(os.system("rosrun {} retrain {} {} {}".format(pkg_name,
-                                                                       os.path.join(assets_path, 'training'),
-                                                                       models_path,
-                                                                       models_path)))
-        self.assertFalse(os.system("rosrun {} evaluate_classifier {} {} {}".format(pkg_name,
-                                                                                   os.path.join(models_path,
-                                                                                                'output_graph.pb'),
-                                                                                   os.path.join(models_path,
-                                                                                                'output_labels.txt'),
-                                                                                   os.path.join(assets_path,
-                                                                                                'verification'))))
-
-
-if __name__ == '__main__':
-    rostest.rosrun("image_recognition_tensorflow", 'test_all', TestRetrainAndEvaluate)
+def test_retrain_and_evaluate():
+    assert 0 == os.system("rosrun {} retrain {} {} {}".format(pkg_name,
+                                                              os.path.join(assets_path, 'training'),
+                                                              models_path,
+                                                              models_path))
+    assert 0 == os.system("rosrun {} evaluate_classifier {} {} {}".format(pkg_name,
+                                                                          os.path.join(models_path, 'output_graph.pb'),
+                                                                          os.path.join(models_path,
+                                                                                       'output_labels.txt'),
+                                                                          os.path.join(assets_path, 'verification')))
