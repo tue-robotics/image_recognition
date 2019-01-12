36cc3e4eae3189eba77b8fbafef6162050b507e6
Rein Appeldoorn <reinzor@gmail.com>
2019-01-12 16:22:02 +0100

    Direct call to script

--- image_recognition_tensorflow/test/test_retrain_and_evaluate.py
+++ image_recognition_tensorflow/test/test_retrain_and_evaluate.py
@@ -4,19 +4,18 @@ import os
 
 import rospkg
 
-pkg_name = 'image_recognition_tensorflow'
-pkg_path = rospkg.RosPack().get_path(pkg_name)
-models_path = os.path.join(pkg_path, 'test/models')
-assets_path = os.path.join(pkg_path, 'test/assets')
-
 
 def test_retrain_and_evaluate():
-    assert 0 == os.system("rosrun {} retrain {} {} {}".format(pkg_name,
-                                                              os.path.join(assets_path, 'training'),
-                                                              models_path,
-                                                              models_path))
-    assert 0 == os.system("rosrun {} evaluate_classifier {} {} {}".format(pkg_name,
-                                                                          os.path.join(models_path, 'output_graph.pb'),
-                                                                          os.path.join(models_path,
-                                                                                       'output_labels.txt'),
-                                                                          os.path.join(assets_path, 'verification')))
+    # Direct call to script because rosrun and packages could not yet be available in environment
+    pkg_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+    models_path = os.path.join(pkg_path, 'test/models')
+    assets_path = os.path.join(pkg_path, 'test/assets')
+    assert 0 == os.system("{} {} {} {}".format(os.path.join(pkg_path, "scripts/retrain"),
+                                               os.path.join(assets_path, 'training'),
+                                               models_path,
+                                               models_path))
+    assert 0 == os.system("{} {} {} {}".format(os.path.join(pkg_path, "scripts/evaluate_classifier"),
+                                               os.path.join(models_path, 'output_graph.pb'),
+                                               os.path.join(models_path,
+                                                            'output_labels.txt'),
+                                               os.path.join(assets_path, 'verification')))
