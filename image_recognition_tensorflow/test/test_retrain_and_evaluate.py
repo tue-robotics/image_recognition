c4ee40a4bd28ff074d7256970cc1fbcff472a3c4
Rein Appeldoorn <reinzor@gmail.com>
2019-01-25 18:16:15 +0100

    try tmp models folder

--- image_recognition_tensorflow/test/test_retrain_and_evaluate.py
+++ image_recognition_tensorflow/test/test_retrain_and_evaluate.py
@@ -8,8 +8,11 @@ from image_recognition_tensorflow.retrain import defaults, main
 
 def test_retrain_and_evaluate():
     pkg_path = rospkg.RosPack().get_path("image_recognition_tensorflow")
-    models_path = os.path.join(pkg_path, 'test/models')
+    models_path = os.path.join(pkg_path, '/tmp/models')
     assets_path = os.path.join(pkg_path, 'test/assets')
+
+    if not os.path.exists(models_path):
+        os.makedirs(models_path)
 
     main(os.path.join(assets_path, "training"), models_path, models_path, defaults.steps, defaults.batch)
     accuracy = evaluate_classifier(os.path.join(models_path, "output_graph.pb"),
