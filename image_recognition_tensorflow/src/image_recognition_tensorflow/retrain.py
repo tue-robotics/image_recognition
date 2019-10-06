5f642539a2102e80f5f154e81e14b199d18aebfc
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-10-06 22:47:08 +0200

    Fix tensorflow2 compatibility

--- image_recognition_tensorflow/src/image_recognition_tensorflow/retrain.py
+++ image_recognition_tensorflow/src/image_recognition_tensorflow/retrain.py
@@ -28,24 +28,24 @@ def mkdir_p(path):
 def main(image_dir, output_dir, steps, batch,
          tfhub_module="https://tfhub.dev/google/imagenet/inception_v3/feature_vector/3",
          flip_left_right=False, random_crop=0, random_scale=0, random_brightness=0):
-    tf.app.flags.FLAGS.image_dir = image_dir
+    tf.compat.v1.app.flags.FLAGS.image_dir = image_dir
 
     mkdir_p(output_dir)
-    tf.app.flags.FLAGS.output_graph = os.path.join(output_dir, 'output_graph.pb')
-    tf.app.flags.FLAGS.output_labels = os.path.join(output_dir, 'output_labels.txt')
+    tf.compat.v1.app.flags.FLAGS.output_graph = os.path.join(output_dir, 'output_graph.pb')
+    tf.compat.v1.app.flags.FLAGS.output_labels = os.path.join(output_dir, 'output_labels.txt')
 
-    tf.app.flags.FLAGS.tfhub_module = tfhub_module
+    tf.compat.v1.app.flags.FLAGS.tfhub_module = tfhub_module
 
-    tf.app.flags.FLAGS.how_many_training_steps = steps
+    tf.compat.v1.app.flags.FLAGS.how_many_training_steps = steps
 
-    tf.app.flags.FLAGS.test_batch_size = batch
-    tf.app.flags.FLAGS.train_batch_size = batch
-    tf.app.flags.FLAGS.validation_batch_size = batch
+    tf.compat.v1.app.flags.FLAGS.test_batch_size = batch
+    tf.compat.v1.app.flags.FLAGS.train_batch_size = batch
+    tf.compat.v1.app.flags.FLAGS.validation_batch_size = batch
 
-    tf.app.flags.FLAGS.flip_left_right = flip_left_right
-    tf.app.flags.FLAGS.random_crop = random_crop
-    tf.app.flags.FLAGS.random_scale = random_scale
-    tf.app.flags.FLAGS.random_brightness = random_brightness
+    tf.compat.v1.app.flags.FLAGS.flip_left_right = flip_left_right
+    tf.compat.v1.app.flags.FLAGS.random_crop = random_crop
+    tf.compat.v1.app.flags.FLAGS.random_scale = random_scale
+    tf.compat.v1.app.flags.FLAGS.random_brightness = random_brightness
 
     # Do not pass additional flags to tf (from rqt and nose)
     sys.argv = [arg for arg in sys.argv if not arg.startswith("-")]
--- image_recognition_tensorflow/src/image_recognition_tensorflow/tf_retrain.py
+++ image_recognition_tensorflow/src/image_recognition_tensorflow/tf_retrain.py
@@ -132,10 +132,10 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-FLAGS = tf.app.flags.FLAGS
+FLAGS = tf.compat.v1.app.flags.FLAGS
 
 # # Architecture
-# tf.app.flags.DEFINE_string('architecture', 'inception_v3',
+# tf.compat.v1.app.flags.DEFINE_string('architecture', 'inception_v3',
 # """Which model architecture to use. 'inception_v3' is the most accurate, but"""
 # """the slowest. For faster or smaller models, chose a MobileNet with the"""
 # """form 'mobilenet_<parameter size>_<input_size>[_quantized]'. For example,"""
@@ -147,77 +147,77 @@ FLAGS = tf.app.flags.FLAGS
 
 
 # Input and output file flags.
-tf.app.flags.DEFINE_string('image_dir', '',
+tf.compat.v1.app.flags.DEFINE_string('image_dir', '',
                            """Path to folders of labeled images.""")
-tf.app.flags.DEFINE_string('output_graph', '/tmp/output_graph.pb',
+tf.compat.v1.app.flags.DEFINE_string('output_graph', '/tmp/output_graph.pb',
                            """Where to save the trained graph.""")
-tf.app.flags.DEFINE_string('intermediate_output_graphs_dir','/tmp/intermediate_graph/',
+tf.compat.v1.app.flags.DEFINE_string('intermediate_output_graphs_dir','/tmp/intermediate_graph/',
                            """Where to save the intermediate graphs.""")
-tf.app.flags.DEFINE_integer('intermediate_store_frequency', 0,
+tf.compat.v1.app.flags.DEFINE_integer('intermediate_store_frequency', 0,
                             """How many steps to store intermediate graph. If "0" then will not store.""")
-tf.app.flags.DEFINE_string('output_labels', '/tmp/output_labels.txt',
+tf.compat.v1.app.flags.DEFINE_string('output_labels', '/tmp/output_labels.txt',
                            """Where to save the trained graph's labels.""")
-tf.app.flags.DEFINE_string('summaries_dir', '/tmp/retrain_logs',
+tf.compat.v1.app.flags.DEFINE_string('summaries_dir', '/tmp/retrain_logs',
                            """Where to save summary logs for TensorBoard.""")
 
 # Details of the training configuration.
-tf.app.flags.DEFINE_integer('how_many_training_steps', 4000,
+tf.compat.v1.app.flags.DEFINE_integer('how_many_training_steps', 4000,
                             """How many training steps to run before ending.""")
-tf.app.flags.DEFINE_float('learning_rate', 0.01,
+tf.compat.v1.app.flags.DEFINE_float('learning_rate', 0.01,
                           """How large a learning rate to use when training.""")
-tf.app.flags.DEFINE_integer('testing_percentage', 10,
+tf.compat.v1.app.flags.DEFINE_integer('testing_percentage', 10,
                             """What percentage of images to use as a test set.""")
-tf.app.flags.DEFINE_integer('validation_percentage', 10,
+tf.compat.v1.app.flags.DEFINE_integer('validation_percentage', 10,
                             """What percentage of images to use as a validation set.""")
-tf.app.flags.DEFINE_integer('eval_step_interval', 10,
+tf.compat.v1.app.flags.DEFINE_integer('eval_step_interval', 10,
                             """How often to evaluate the training results.""")
-tf.app.flags.DEFINE_integer('train_batch_size', 100,
+tf.compat.v1.app.flags.DEFINE_integer('train_batch_size', 100,
                             """How many images to train on at a time.""")
-tf.app.flags.DEFINE_integer('test_batch_size', -1,
+tf.compat.v1.app.flags.DEFINE_integer('test_batch_size', -1,
     """How many images to test on. This test set is only used once, to evaluate
     the final accuracy of the model after training completes.
     A value of -1 causes the entire test set to be used, which leads to more
     stable results across runs""")
-tf.app.flags.DEFINE_integer('validation_batch_size', 100,
+tf.compat.v1.app.flags.DEFINE_integer('validation_batch_size', 100,
     """How many images to use in an evaluation batch. This validation set is
     used much more often than the test set, and is an early indicator of how
     accurate the model is during training.
     A value of -1 causes the entire validation set to be used, which leads to
     more stable results across training iterations, but may be slower on large
     training sets.""")
-tf.app.flags.DEFINE_boolean('print_misclassified_test_images', False,
+tf.compat.v1.app.flags.DEFINE_boolean('print_misclassified_test_images', False,
     """Whether to print out a list of all misclassified test images.""")
 
 
-tf.app.flags.DEFINE_string(
+tf.compat.v1.app.flags.DEFINE_string(
     'bottleneck_dir', '/tmp/bottleneck',
     """Path to cache bottleneck layer values as files.""")
-tf.app.flags.DEFINE_string('final_tensor_name', 'final_result',
+tf.compat.v1.app.flags.DEFINE_string('final_tensor_name', 'final_result',
                            """The name of the output classification layer in"""
                            """ the retrained graph.""")
 
 # Controls the distortions used during training.
-tf.app.flags.DEFINE_boolean(
+tf.compat.v1.app.flags.DEFINE_boolean(
     'flip_left_right', False,
     """Whether to randomly flip half of the training images horizontally.""")
-tf.app.flags.DEFINE_integer(
+tf.compat.v1.app.flags.DEFINE_integer(
     'random_crop', 0,
     """A percentage determining how much of a margin to randomly crop off the"""
     """ training images.""")
-tf.app.flags.DEFINE_integer(
+tf.compat.v1.app.flags.DEFINE_integer(
     'random_scale', 0,
     """A percentage determining how much to randomly scale up the size of the"""
     """ training images by.""")
-tf.app.flags.DEFINE_integer(
+tf.compat.v1.app.flags.DEFINE_integer(
     'random_brightness', 0,
     """A percentage determining how much to randomly multiply the training"""
     """ image input pixels up or down by.""")
 
-tf.app.flags.DEFINE_string('tfhub_module', 'https://tfhub.dev/google/imagenet/inception_v3/feature_vector/1', """
+tf.compat.v1.app.flags.DEFINE_string('tfhub_module', 'https://tfhub.dev/google/imagenet/inception_v3/feature_vector/1', """
     Which TensorFlow Hub module to use. See https://github.com/tensorflow/hub/blob/master/docs/tutorials/image_retraining.md
     for some publicly available ones.""")
 
-tf.app.flags.DEFINE_string('saved_model_dir', '',
+tf.compat.v1.app.flags.DEFINE_string('saved_model_dir', '',
                            """Where to save the exported graph.""")
 
 # These are all parameters that are tied to the particular model architecture
