3cfb4d4810874d58069ee5fb34e5730a92aa9c2a
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-02-03 16:36:04 +0100

    (skybiometry) fix test

--- image_recognition_skybiometry/test/test_face_properties.py
+++ image_recognition_skybiometry/test/test_face_properties.py
@@ -12,6 +12,7 @@ from image_recognition_skybiometry.skybiometry import Skybiometry
 def test_face_properties():
     test_key = '69efefc20c7f42d8af1f2646ce6742ec'
     test_secret = '5fab420ca6cf4ff28e7780efcffadb6c'
+    test_namespace = 'techunited'
 
     def age_is_female_from_asset_name(asset_name):
         age_str, gender_str = re.search("age_(\d+)_gender_(\w+)", asset_name).groups()
@@ -21,7 +22,7 @@ def test_face_properties():
     images_gt = [(cv2.imread(os.path.join(assets_path, asset)), age_is_female_from_asset_name(asset))
                  for asset in os.listdir(assets_path)]
 
-    estimations = Skybiometry(test_key, test_secret).get_face_properties([image for image, _ in images_gt], 10.0)
+    estimations = Skybiometry(test_key, test_secret, test_namespace).get_face_properties([image for image, _ in images_gt], 10.0)
     for (_, (age_gt, is_female_gt)), face_property in zip(images_gt, estimations):
         age = int(face_property.age_est.value)
         is_female = face_property.gender.value == "female"
