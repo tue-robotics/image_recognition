25c8599751c6992599be9696df89aba29fa3d76b
Arpit Aggarwal <ar13pit@gmail.com>
2022-03-08 19:41:42 +0100

    rename pytorch to age_gender

--- image_recognition_age_gender/setup.py
+++ image_recognition_age_gender/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['image_recognition_pytorch'],
+    packages=['image_recognition_age_gender'],
     package_dir={'': 'src'}
 )
 
