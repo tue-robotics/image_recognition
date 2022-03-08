5355c890cb3297b29ca9a23e3d398d02b7d356ad
Arpit Aggarwal <ar13pit@gmail.com>
2022-03-08 20:01:12 +0100

    test(image_recognition_age_gender): Fix test

--- image_recognition_age_gender/test/test_face_properties.py
+++ image_recognition_age_gender/test/test_face_properties.py
@@ -7,16 +7,15 @@ from future.moves.urllib.request import urlretrieve
 
 import cv2
 import rospkg
-from image_recognition_keras.age_gender_estimator import AgeGenderEstimator
+from image_recognition_age_gender.age_gender_estimator import AgeGenderEstimator
 
 
 def test_face_properties():
-    local_path = "/tmp/age_gender_weights.hdf5"
+    local_path = os.path.join(os.path.expanduser('~/data/pytorch_models'), "best-epoch47-0.9314.onnx")
 
     if not os.path.exists(local_path):
-        http_path = "https://github.com/tue-robotics/image_recognition/releases/download/" \
-                    "image_recognition_keras_face_properties_weights.28-3.73/" \
-                    "image_recognition_keras_face_properties_weights.28-3.73.hdf5"
+        http_path = "https://github.com/Nebula4869/PyTorch-gender-age-estimation/raw/" \
+                    "038331d26fc1fbf24d00365d0eb9d0e5e828dda6/models-2020-11-20-14-37/best-epoch47-0.9314.onnx"
         urlretrieve(http_path, local_path)
         print("Downloaded weights to {}".format(local_path))
 
@@ -24,7 +23,7 @@ def test_face_properties():
         age_str, gender_str = re.search("age_(\d+)_gender_(\w+)", asset_name).groups()
         return int(age_str), gender_str == "female"
 
-    assets_path = os.path.join(rospkg.RosPack().get_path("image_recognition_keras"), 'test/assets')
+    assets_path = os.path.join(rospkg.RosPack().get_path("image_recognition_age_gender"), 'test/assets')
     images_gt = [(cv2.imread(os.path.join(assets_path, asset)), age_is_female_from_asset_name(asset))
                  for asset in os.listdir(assets_path)]
 
