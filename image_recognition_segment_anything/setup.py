0287634cc562ec94b8cd56663081cf11625e62a1
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-05-30 19:48:49 +0200

    [segment_anything] new pkg

---
+++ image_recognition_segment_anything/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['image_recognition_segment_anything'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
