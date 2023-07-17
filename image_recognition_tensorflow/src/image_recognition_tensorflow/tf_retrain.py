f8cd0355833c68b52c389b0b891ce3175e2879bb
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-07-17 13:32:33 +0200

    (tensorflow) make compatible with tensorflow>=2.13

--- image_recognition_tensorflow/src/image_recognition_tensorflow/tf_retrain.py
+++ image_recognition_tensorflow/src/image_recognition_tensorflow/tf_retrain.py
@@ -115,7 +115,7 @@ from six.moves import urllib
 import tensorflow as tf
 
 from image_recognition_tensorflow.MovingAvgQuantize import MovingAvgQuantize
-from tensorflow.python.framework import graph_util
+from tensorflow.python.framework import convert_to_constants
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.platform import gfile
 from tensorflow.python.util import compat
@@ -949,7 +949,7 @@ def add_evaluation_step(result_tensor, ground_truth_tensor):
 
 
 def save_graph_to_file(sess, graph, graph_file_name):
-  output_graph_def = graph_util.convert_variables_to_constants(
+  output_graph_def = convert_to_constants.convert_variables_to_constants(
       sess, graph.as_graph_def(), [FLAGS.final_tensor_name])
 
   with gfile.FastGFile(graph_file_name, 'wb') as f:
