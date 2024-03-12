a63c76e4d3b6371c13f5494b2dadb08ccc7d3f8b
iasonth95 <iasonth95@gmail.com>
2024-03-12 21:34:12 +0100

    updated scripts and src facenet model according to code review

--- image_recognition_face_recognition/src/image_recognition_face_recognition/facenet_recognition.py
+++ image_recognition_face_recognition/src/image_recognition_face_recognition/facenet_recognition.py
@@ -1,11 +1,14 @@
-import psutil
+from typing import List, Tuple, Union
 import rospy
 import torch
 from facenet_pytorch import MTCNN, InceptionResnetV1
-from facenet_pytorch.models.mtcnn import MTCNN
+import numpy as np
 
 
 class TrainedFace:
+    """
+    This class serves as a custom struct to store information of people we recognized
+    """
     def __init__(self, label):
         """
         A custom struct to store the names and the embedded representations (tensors) of people
@@ -26,41 +29,46 @@ class TrainedFace:
         return self.representations
 
 
-class Facenet_recognition:
+class FacenetRecognition:
+    """
+    This class handles the recognition using the Facenet model.
+    """
     def __init__(self):
         """
         Constructor for the list which contains the TrainedFace structure
         """
         self._trained_faces = []
-
-    def print_memory_usage(self, label):
-        """
-        Prints the memory usage as a metric
-        """
-        mem_info = psutil.Process().memory_info()
-        rospy.loginfo(
-            f"{label: <10}: vms={mem_info.vms / (1024 * 1024)}, rss={mem_info.rss / (1024 * 1024)}"
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        rospy.loginfo(f"Running on device: {self.device}")
+        self.mtcnn = MTCNN(
+            keep_all=True,
+            image_size=160,
+            margin=0,
+            min_face_size=20,
+            thresholds=[0.6, 0.7, 0.7],
+            factor=0.709,
+            post_process=True,
+            device=self.device,
         )
-
-    def face_detection(self, img):
+        
+    def face_detection(self, img: np.ndarray) -> List[Union[Tuple[float, float, float, float], None]]:
         """
         Returns the index of the trained face
 
         :param img: inpute image
         :return: the bounding boxes of coordinations of the faces it detects
         """
-        detector = MTCNN(keep_all=True)
+   
         # Keep the landmarks for future use
-        boxes, _, landmarks = detector.detect(img, landmarks=True)
+        boxes, _, landmarks = self.mtcnn.detect(img, landmarks=True)
         return boxes
 
-    def _get_dists(self, embeddings):
+    def _get_dists(self, embeddings: List[torch.Tensor]) -> Tuple[List[float], List[str]]:
         """
         Returns min l2 distance of the identified face (compared with the database of know faces)
 
         :param embeddings: the embedded representation(s) of a face(s)
-        :return: the min distance(s) of the embedded vector compared with the database faces
-        :return: the corresponding label(s)
+        :return: the min distance(s) of the embedded vector compared with the database faces from the corresponding label(s)
         """
         dist_per_emb_final = []
         dist = []
@@ -100,7 +108,7 @@ class Facenet_recognition:
 
         return min_value_list_per_emb, labelling
 
-    def detection_recognition(self, img, labels, train):
+    def detection_recognition(self, img: np.ndarray, labels: List[str], train: bool) -> Tuple[List[float], List[str]]:
         """
         Returns min l2 distance of the identified face (compared with the database of know faces)
 
@@ -112,38 +120,20 @@ class Facenet_recognition:
         :return: the min distance(s) of the embedded vector compared with the database faces
         :return: the corresponding label(s)
         """
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        rospy.loginfo(f"Running on device: {device}")
+        resnet = InceptionResnetV1(pretrained="vggface2").eval().to(self.device)
 
-        mtcnn = MTCNN(
-            keep_all=True,
-            image_size=160,
-            margin=0,
-            min_face_size=20,
-            thresholds=[0.6, 0.7, 0.7],
-            factor=0.709,
-            post_process=True,
-            device=device,
-        )
-
-        resnet = InceptionResnetV1(pretrained="vggface2").eval().to(device)
-
-        x_aligned = mtcnn(img)
-        print(type(x_aligned))
-        print(x_aligned.size(), "x_aligned size (1st NN output)")
+        x_aligned = self.mtcnn(img)
         x_aligned = x_aligned.cuda()  # add this line
         embeddings = resnet(x_aligned).detach().cpu()
         rospy.loginfo(f"{embeddings.size()}, {type(embeddings)}, embeddings size")
 
         if not self._trained_faces:
-            nam = 0
-            for emb in embeddings:
+            for nam, emb in enumerate(embeddings):
                 index = self._get_trained_face_index(labels[nam])
                 if index == -1:
                     self._trained_faces.append(TrainedFace(labels[nam]))
 
                 self._trained_faces[index].representations.append(emb)
-                nam = nam + 1
 
         # try:
         dist, labelling = self._get_dists(embeddings)
@@ -152,16 +142,14 @@ class Facenet_recognition:
         rospy.loginfo(f"{labels[0]}, {labelling}, label[0],labelling")
 
         if train:
-            idx_label = 0
-            for emb in embeddings:
+            for idx_label, emb in enumerate(embeddings):
                 rospy.loginfo(f"{idx_label}, idx_label")
                 self.train(emb, labelling[idx_label])
-                idx_label = idx_label + 1
         # except:
         rospy.loginfo(f"{len(self._trained_faces)}")
         return dist, labelling
 
-    def _get_trained_face_index(self, label):
+    def _get_trained_face_index(self, label: str) -> int:
         """
         Returns the index of the trained face
 
@@ -173,7 +161,7 @@ class Facenet_recognition:
                 return i
         return -1
 
-    def train(self, face_representation, name):
+    def train(self, face_representation: np.ndarray, name: str) -> None:
         """
         Adds a face to the trained faces, creates a vector representation and adds this
 
@@ -191,11 +179,3 @@ class Facenet_recognition:
             rospy.loginfo(
                 f"Label: {trained_face.get_label()}, Representations: {len(trained_face.get_representations())}"
             )
-
-
-if __name__ == "__main__":
-    """
-    Runs the whole code. It is not needed if it runs from a ros node.
-    """
-    face_detection = Facenet_recognition()
-    face_detection.detection_recognition()
