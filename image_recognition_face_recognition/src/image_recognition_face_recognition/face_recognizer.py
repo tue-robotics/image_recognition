f2632956d46bb15e9057c9fe625e61aa0100fb02
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2025-01-21 21:16:11 +0100

    (face) update categorial distribution working almost
    Need to use the aligned face instead of the plain crop

--- image_recognition_face_recognition/src/image_recognition_face_recognition/face_recognizer.py
+++ image_recognition_face_recognition/src/image_recognition_face_recognition/face_recognizer.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import pickle
+import uuid
 from dataclasses import dataclass, field
 from os import PathLike
 from pathlib import Path
@@ -121,7 +122,7 @@ class RecognizedFace:
         """
         A Recognized face in an image
 
-        :param image: The original image
+        :param image: The original cropped image
         :param roi: Region of Interest
         """
         self._image = image
@@ -156,10 +157,12 @@ class FaceRecognizer:
     This class handles the recognition using the Facenet model.
     """
 
-    def __init__(self):
+    def __init__(self, distance_threshold: float = 1.0) -> None:
         """
         Constructor for the list which contains the TrainedFace structure
         """
+        self._distance_threshold: float = distance_threshold
+
         self._trained_faces: List[TrainedFace] = []
         self._device = torch.device(
             "cuda:0" if torch.cuda.is_available() else "cpu")
@@ -177,42 +180,37 @@ class FaceRecognizer:
         self._resnet = InceptionResnetV1(
             pretrained="vggface2", device=self._device).eval()
 
-    def _update_with_categorical_distribution(self, recognition: RecognizedFace, labels: List[str]) -> RecognizedFace:
+    def _update_with_categorical_distribution(self, recognition: RecognizedFace) -> RecognizedFace:
         """
         Update the recognition with a categorical distribution of the trained faces
 
         :param recognition: Input recognition
         :return: Output recognition with an updated categorical distribution
         """
-        if self._trained_faces:
-            # Try to get a representation of the detected face
-            recognition_embedding = None
-            try:
-                recognition_embedding = self._get_embedding(
-                    torch.stack([recognition], dim=0)).squeeze()
-            except Exception as e:
-                rospy.logerr(f"Error getting the embedding: {e}")
+        # Try to get a representation of the detected face
+        recognition_embedding = None
+        try:
+            recognition_embedding = self._get_embedding(
+                torch.stack([recognition.image], dim=0)).squeeze()
+        except Exception as e:
+            rospy.logerr(f"Error getting the embedding: {e}")
 
-            if recognition_embedding is not None:
-                # Calculate the L2 distance between the embedding and all the stored representations.
-                recognition.l2_distances = [
-                    L2Distance(self._get_min_l2_distance(
-                        face.representations, recognition_embedding), face.label)
-                    for face in self._trained_faces
-                ]
-        else:
-            recognition_embedding = None
-            try:
-                recognition_embedding = self._get_embedding(
-                    torch.stack([recognition], dim=0)).squeeze()
-            except Exception as e:
-                rospy.logerr(f"Error getting the embedding: {e}")
+        if recognition_embedding is not None:
+            # Calculate the L2 distance between the embedding and all the stored representations.
+            recognition.l2_distances = [
+                L2Distance(self._get_min_l2_distance(
+                    face.representations, recognition_embedding), face.label)
+                for face in self._trained_faces
+            ]
+            recognition.l2_distances.sort(key=lambda x: x.distance)
 
-            index = self._get_trained_face_index(labels[0])
-            if index == -1:
-                self._trained_faces.append(TrainedFace(labels[0]))
-            self._trained_faces[index].representations.append(
-                recognition_embedding)
+        if self._distance_threshold > 0 and (not recognition.l2_distances or recognition.l2_distances[0].distance > self._distance_threshold):
+            # Store the new face under a new label
+            label = self._generate_label()
+            trained_face = TrainedFace(label)
+            trained_face.representations.append(recognition_embedding)
+            self._trained_faces.append(trained_face)
+            recognition.l2_distances.insert(0, L2Distance(0, label))
 
         return recognition
 
@@ -235,7 +233,16 @@ class FaceRecognizer:
         """
         return self._resnet(bgr_image.to(self._device)).detach().cpu().numpy()
 
-    def detect(self, img: np.ndarray, labels: List[str]) -> List[RecognizedFace]:
+    @staticmethod
+    def _generate_label() -> str:
+        """
+        Generates a new UUID label
+
+        :return: The new label
+        """
+        return str(uuid.uuid4())
+
+    def detect(self, img: np.ndarray) -> List[RecognizedFace]:
         """Detect faces in an image
 
         :param img: input image in bgr
@@ -243,26 +250,12 @@ class FaceRecognizer:
         """
 
         bboxes, probs, _ = self._mtcnn.detect(img)
-        pred_aligned, batch_probs = self._mtcnn(img)
-        pred_aligned = pred_aligned.cuda()
+
         # Extract faces from the bounding boxes
         recognized_faces = [self._get_recognized_face(
             img, bbox) for bbox in bboxes]
-        faceone = recognized_faces[0].image
-        # TODO: Implement the update with categorical distribution
         recognized_faces = [self._update_with_categorical_distribution(
-            face, labels) for face in pred_aligned]
-        # faces_only = [face._image for face in recognized_faces]
-        # faces_only = [torch.from_numpy(face_only) for face_only in faces_only]
-        # faces_only = torch.stack(faces_only, axis=0)
-
-        embeddings = self._resnet(pred_aligned).detach().cpu()
-        # embeddings = self._get_embedding(faces_only)
-
-        for idx_label, emb in enumerate(embeddings):
-            rospy.loginfo(f"{idx_label}, idx_label")
-            self.train(emb, labelling[idx_label])
-        rospy.loginfo(f"{len(self._trained_faces)}")
+            recognized_face) for recognized_face in recognized_faces]
 
         return recognized_faces
 
